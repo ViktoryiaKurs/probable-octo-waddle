27900193565dcf8e4cafd0c003f4e0fbb50f2161
hacker2023beginer <tsurkovlados@gmail.com>
2025-04-10 20:40:50 +0300

    Исправил (вроде)

--- binary_search/untitled/src/BinSort.java
+++ binary_search/untitled/src/BinSort.java
@@ -1,22 +1,21 @@
 import java.util.ArrayList;
 
 public class BinSort {
-    public int Search(int n, ArrayList<Integer> mas){
+    public int search(int target, ArrayList<Integer> mas){
         int left = 0;
         int counter = 0;
         int right = mas.size() - 1;
-        int founded = n + 1;
-        while (founded != n){
+        int middle = (right + left) / 2;
+        while (true){
             counter++;
-            if (mas.get((right + left) / 2) == n){
-                founded = n;
-                continue;
+            if (mas.get(middle) == n){
+                break;
             }
-            else if (mas.get((right + left) / 2) < n){
-                left = (right + left) / 2 + 1;
+            else if (mas.get(middle) < n){
+                left = middle + 1;
             }
-            else if (mas.get((right + left) / 2) > n){
-                right = (right + left) / 2 - 1;
+            else if (mas.get(middle) > n){
+                right = middle - 1;
             }
         }
         return counter;
