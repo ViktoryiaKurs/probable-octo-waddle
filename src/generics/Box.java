60dfb21a37e6be59f4fc6e7b93f2f778baae2fda
ViktoryiaKurs <vikakurs1998@gmail.com>
2025-04-12 19:57:19 +0200

    Change format

--- src/generics/Box.java
+++ src/generics/Box.java
@@ -1,31 +1,37 @@
 package generics;
 
-public class Box <T>{
+public class Box<T> {
     private T value;
-    public  void put(T item){
-        if ( item==null){
+
+    public void put(T item) {
+        if (item == null) {
             System.out.println("The element is empty");
             return;
         }
         this.value = item;
     }
-    public T getValue(){
+
+    public T getValue() {
         return value;
     }
-    public boolean isEmpty(){
-        return value==null;
+
+    public boolean isEmpty() {
+        return value == null;
     }
-    public static <T> Box<T> fromArray(T[] array){
-if (array==null || array.length==0){
-    return null;
-}
-    Box<T> box= new Box<>();
-    box.put(array[0]);
-    return box;}
-public boolean isSameType (Box <?> other){
-     if (this.value==null || other==null || other.getValue()==null){
-         return false;
-     }
-    return this.value.getClass().equals(other.getValue().getClass());
-}
+
+    public static <T> Box<T> fromArray(T[] array) {
+        if (array == null || array.length == 0) {
+            return null;
+        }
+        Box<T> box = new Box<>();
+        box.put(array[0]);
+        return box;
+    }
+
+    public boolean isSameType(Box<?> other) {
+        if (this.value == null || other == null || other.getValue() == null) {
+            return false;
+        }
+        return this.value.getClass().equals(other.getValue().getClass());
+    }
 }
